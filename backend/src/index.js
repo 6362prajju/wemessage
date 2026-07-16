ad88d9f758fc6bc041ddbc7ce802bda3b31f222a
6362prajju <shreekarpuranik3@gmail.com>
2026-07-16 10:08:53 +0530

    Add Clerk authentication and MongoDB connection
    - Integrated @clerk/express for user authentication in the backend.
    - Established MongoDB connection with error handling.
    - Created User and Message models for database interactions.
    - Updated frontend to utilize @clerk/react for user sign-in and sign-up.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,24 @@
-import express from "express"
-import "dotenv/config.js"
+import express from "express";
+import "dotenv/config.js";
+import User from "./models/user.model.js";
+import {connectDB} from "./lib/db.js";
+import  {clerkMiddleware} from "@clerk/express";
+import cors from "cors"; 
 
 const app = express()
 const PORT = process.env.PORT 
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`))
+const FRONTEND_URL = process.env.FRONTEND_URL
+
+app.use(express.json()); 
+app.use(cors({origin : FRONTEND_URL, credentials : true})); 
+app.use(clerkMiddleware());
+
+app.get("/health",(req,res) => {
+    res.status(200).json({ok : true});
+})
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running up and on port:",PORT);
+}
+)
