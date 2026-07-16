dac4c71d9751d416aae7d0466c609e99448057c0
6362prajju <shreekarpuranik3@gmail.com>
2026-07-16 20:01:47 +0530

    feat: add webhook handler for Clerk events in clerk.webhook.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors"; 
-import job  from "./lib/cron.js";
 
 import "dotenv/config.js";
 import fs from "fs";
@@ -10,6 +9,9 @@ import  {clerkMiddleware} from "@clerk/express";
 
 import User from "./models/user.model.js";
 import {connectDB} from "./lib/db.js";
+import job  from "./lib/cron.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 
 const app = express()
@@ -18,6 +20,8 @@ const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use("/api/webhooks/clerk", express.raw({type : "application/json"}), clerkWebhook); 
 
 app.use(express.json()); 
 app.use(cors({origin : FRONTEND_URL, credentials : true})); 
